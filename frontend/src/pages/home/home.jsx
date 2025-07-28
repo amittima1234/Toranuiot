4e000bb30212ac19c3c88db6defd9468d7a1507d
amittima1234 <amittima1234@gmail.com>
2025-07-28 12:08:38 +0300

    change to vue from react

--- frontend/src/pages/home/home.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './Home.module.css'
-
-export default function Home() {
-  return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>ברוך הבא</h1>
-    </div>
-  )
-}
